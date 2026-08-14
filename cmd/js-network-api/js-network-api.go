cbfb5479c7b9d8a1a24975bb525bc6b5e7a94eb3
afsuyadi <afsuyadi@gmail.com>
2026-08-14 08:40:25 +0700

    rename files and dirs for go standards

--- cmd/js-network-api/js-network-api.go
+++ cmd/js-network-api/js-network-api.go
@@ -9,7 +9,7 @@ import (
 	"encoding/csv";
 	"os";
 	"time";
-	urlReader "github.com/afsuyadi/grab-careers-parser-2/internal/ioRead";
+	urlReader "github.com/afsuyadi/grab-careers-parser-2/pkg/urls-reader";
 
 )
 
